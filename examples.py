26c3bfb7eb32bf75989391f721647ff7929e4d47
Ashley Luhrs <aluhrs@gmail.com>
2014-02-14 17:49:45 -0800

    Up to palindrome

--- examples.py
+++ examples.py
@@ -4,9 +4,6 @@ def multiply_list(l):
         return 1
     return l[0] * multiply_list(l[1:])
 
-list = [1,2,3,4]
-print multiply_list(list)
-
 # Return the factorial of n
 def factorial(n):
     if n == 0:
@@ -15,14 +12,10 @@ def factorial(n):
     #n - 1! = n-1 * n-2!
 
 # Count the number of elements in the list l
-
-
 def count(l):
     if l == []:
         return 0
     return len(l[0]) + count(l[1:])
-
-
 
 # Sum all of the elements in a list
 def sum_list(l):
@@ -32,22 +25,33 @@ def sum_list(l):
 
 # Reverse a list without slicing or loops
 def reverse(l):
-    pass
+    if l == []:
+        return l
+    return reverse(l[1:]) + [l[0]]
 
 # Fibonacci returns the nth fibonacci number. The nth fibonacci number is
 # defined as fib(n) = fib(n-1) + fib(n-2)
 def fibonacci(n):
-    pass
+    if n <= 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
 
 # Finds the item i in the list l.... RECURSIVELY
 def find(l, i):
-    
+    if l[0] == i:
+        return i
+    if l == []:
+        return none
+    # give the function the new list and the item to find again    
+    return find(l[1:],i)
 
 # Determines if a string is a palindrome
 def palindrome(some_string):
     return False
 
-# Given the width and height of a sheet of paper, and the number of times to fold it, return the final dimensions of the sheet as a tuple. Assume that you always fold in half along the longest edge of the sheet.
+# Given the width and height of a sheet of paper, and the number of times 
+# to fold it, return the final dimensions of the sheet as a tuple. 
+# Assume that you always fold in half along the longest edge of the sheet.
 def fold_paper(width, height, folds):
     return (0, 0)
 
@@ -57,5 +61,5 @@ def count_up(target, n):
     return
 
 list = [1,2,3,4]
-print count(list)  
+print fibonacci(4)  
 
